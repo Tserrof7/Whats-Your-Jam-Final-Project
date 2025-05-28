6dcb9a045a81114902ef4222ea3e64c446fade6d
Tserrof7 <forrest.margulies@gmail.com>
2025-05-27 21:47:01 -0700

    adding input box with wokring tabs

--- app.py
+++ app.py
@@ -18,36 +18,25 @@ app.layout = html.Div([
        
     html.Div(id='tab-content'),
     
-
-    html.Div(
-        dcc.Input(
-      id="song_input",
-      type='text',
-      placeholder='song name'
-        )
-  )
-
-
-    
-],style={"marginLeft": "30px", "marginRight": "30px"})
+],style={"margin-left": "30px", "margin-right": "30px"})
 
 
 
-@callback(Output('tab-content', 'children'), Input('tabs', 'value'))
+@callback(Output('tab-content', 'children'),Input('tabs', 'value'))
 def render_tab(tab):
     if tab == 'tab-recommender':
         return html.Div([
             dbc.Row([
                 dbc.Col([
-                        dcc.Input(
+                        dbc.Input(
                             id="song_name",
                             type="text",
                             placeholder="song name",
-                            style={"text-center"}),
+                            style={}),
 
     
-    html.Div(id="output"),
-                ])
+    # html.Div(id="output"),
+                ]),
             ])
         ])
     elif tab == 'tab-eda':
@@ -56,21 +45,21 @@ def render_tab(tab):
                 dbc.Col([
                     dbc.Carousel(
                         items=[
-                            {"key":"1","src":"assets/insert_image_path", "caption":"insert caption", "img_style":{"max-height":"250"}},
-                             {"key":"2","src":"assets/insert_image_path",
-                             "header":"insert header","caption":"insert caption","img_style":{"max_height":"250"} }
+                            {"key":"1","src":"assets/Boston-cream-donut.png", "caption":"This is a boston cream donut", "img_style":{"max-height":"500px"}},
+                             {"key":"2","src":"assets/assets/f8b76d1883e09d74.png",
+                             "header":"minecraft skin","caption":"This is my minecraft skin","img_style":{"max_height":"500px"} }
                         ]
                     )
                  ])
             ])
         ])
 
-@callback(
-    Output("out-all-types", "children"),
-    [Input("input_{}".format(_), "value") for _ in ALLOWED_TYPES],
-)
-def cb_render(*vals):
-    return " | ".join((str(val) for val in vals if val))
+# @callback(
+#     Output("out-all-types", "children"),
+#     [Input("input_{}".format(_), "value") for _ in ALLOWED_TYPES],
+# )
+# def cb_render(*vals):
+#     return " | ".join((str(val) for val in vals if val))
 
 
 
